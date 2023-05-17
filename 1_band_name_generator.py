1971dd040b8468a9c970b4321a010c482e474158
Matěj Svoboda <svobodamatej97@gmail.com>
2023-05-17 22:14:04 +0200

    Programm now print suggested name of a band. Project completed.

--- 1_band_name_generator.py
+++ 1_band_name_generator.py
@@ -2,3 +2,5 @@ print("Hello! Welcome to Band Name Generator!")
 
 city = input("Where did you grow up?\n")
 pet = input("Where did you grow up?\n")
+
+print(f"Your band could be named {city}{pet}! :-)")
