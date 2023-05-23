ac3dc2b2bbd7bccf5a7f270a6e29f7af50ee9bec
Matěj Svoboda <svobodamatej97@gmail.com>
2023-05-23 20:44:44 +0200

    Add whole directory with even not started projects.

---
+++ 5_password_generator.py
@@ -0,0 +1,17 @@
+#Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+number_of_letters = int(input("How many letters would you like in your password?\n")) 
+number_of_numbers = int(input("How many numbers would you like in your password?\n"))
+number_of_symbols = int(input("How many symbols would you like in your password?\n"))
+
+password = []
+for letter in number_of_letters:
+    password.append(random.choice(number_of_letters))
+
+print(password)
