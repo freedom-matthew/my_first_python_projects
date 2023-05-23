80eaa2a828acc79764f242c8cd927975ada05d37
Matěj Svoboda <svobodamatej97@gmail.com>
2023-05-23 19:43:13 +0200

    Add possibility to iterate over chosen word and print underscores.

--- 6_hangman.py
+++ 6_hangman.py
@@ -1,13 +1,26 @@
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
-print(chosen_word)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in chosen_word:
+    display += "_"
+print(display)
+
 guess = input("Guess a letter: ").lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if guess in chosen_word:
-    print("true")
-else:
-    print("false")
+
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
