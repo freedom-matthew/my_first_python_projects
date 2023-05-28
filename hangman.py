46e928541006f4426d386f00de4d3418ee7b7a5e
Matěj Svoboda <svobodamatej97@gmail.com>
2023-05-28 11:45:31 +0200

    Debugging. Hangman completed.

--- hangman.py
+++ hangman.py
@@ -2,13 +2,15 @@ import random
 from hangman_art import logo, stages
 from hangman_words import word_list
 
+print(logo)
+
+# Get random word from a list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
+# Init of a game
 end_of_game = False
 lives = 6
-
-print(logo)
 
 display = []
 for _ in range(word_length):
@@ -17,31 +19,30 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    # Check if user has entered same letter
     if guess in display:
-        print(f"You've already guessed {guess}")
+        print(f"You've already guessed {guess}...")
 
-    #Check guessed letter
+    # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+
         if letter == guess:
             display[position] = letter
 
-    #Check if user is wrong.
+    # Check if user is wrong
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
-        
         lives -= 1
+
         if lives == 0:
             end_of_game = True
             print("You lose.")
 
-    #Join all the elements in the list and turn it into a String.
+    # Join all the elements in the list and turn it into a string
     print(f"{' '.join(display)}")
 
-    #Check if user has got all letters.
+    # Check if user has got all letters
     if "_" not in display:
         end_of_game = True
         print("You win.")
