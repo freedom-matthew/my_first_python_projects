a69e21acefa1cc4cd905d670bea9df3aec781ce7
Matěj Svoboda <svobodamatej97@gmail.com>
2023-06-01 21:59:06 +0200

    Optimalization done, 2 functions fused to 1.

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -10,25 +10,15 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    for letter in start_text:
         position = alphabet.index(letter)
+        if cipher_direction == "decode":
+            shift_amount *= -1
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}.")
 
 
-def decrypt(cipher_text, shift_amount):
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        plain_text += alphabet[new_position]
-    print(f"The decoded text is {plain_text}")
-
-
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
