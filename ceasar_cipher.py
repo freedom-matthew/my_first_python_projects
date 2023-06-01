1fed9d957c5be8cc4327839747907ebe40ffe7e0
Matěj Svoboda <svobodamatej97@gmail.com>
2023-06-01 20:32:20 +0200

    "Added decrypt function"

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -1,6 +1,8 @@
 alphabet = [
-   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
-   'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -11,13 +13,22 @@ shift = int(input("Type the shift number:\n"))
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
-      position = alphabet.index(letter)
-      new_position = position + shift_amount
-      if new_position > 26:
-         new_position - 26
-      new_letter = alphabet[new_position]
-      cipher_text += new_letter
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
 
 
-encrypt(plain_text=text, shift_amount=shift)
+def decrypt(cipher_text, shift_amount):
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(cipher_text=text, shift_amount=shift)
