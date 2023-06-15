a338caad51aae6f8beb2679ff096605af28495b0
Matěj Svoboda <svobodamatej97@gmail.com>
2023-06-15 21:59:21 +0200

    First concept of calc done. Kinda satisfied.

---
+++ calculator/main.py
@@ -0,0 +1,37 @@
+from art import logo
+
+print(logo)
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+num1 = int(input("What's the first number?: "))
+
+should_continue = True
+while should_continue:
+    operation_symbol = input("Pick an operation (+, -, *, /): ")
+    num2 = int(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
