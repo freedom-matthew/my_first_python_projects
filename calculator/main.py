5597a201398795039e9302b3b5c6ea61838a8c63
Matěj Svoboda <svobodamatej97@gmail.com>
2023-06-15 22:11:01 +0200

    "Added functionality to pass multiple inputs."

--- calculator/main.py
+++ calculator/main.py
@@ -35,3 +35,8 @@ while should_continue:
     answer = calculation_function(num1, num2)
 
     print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+        num1 = answer
+    else:
+        should_continue = False
